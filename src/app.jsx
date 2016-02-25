b628388c4bae162beaf4cf64a3d16147618c56ff
Joseph McElroy <phoey1@gmail.com>
2016-02-25 08:45:13 +0000

    updating to searchkit 0.7

--- src/app.jsx
+++ src/app.jsx
@@ -1,46 +1,64 @@
 import * as React from "react";
+import * as _ from "lodash";
 
 import {
 	SearchkitManager,
 	SearchkitProvider,
 	SearchBox,
+	RefinementListFilter,
 	Hits
 } from "searchkit";
 
 require("./index.scss");
 
-const host = "https://d78cfb11f565e845000.qb0x.com/movies"
+const host = "http://demo.searchkit.co/api/movies"
 const sk = new SearchkitManager(host, {
   multipleSearchers:false
 })
 
-class MovieHits extends Hits {
-	renderResult(result) {
-		let url = "http://www.imdb.com/title/" + result._source.imdbId
-		return (
-			<div className={this.bemBlocks.item().mix(this.bemBlocks.container("item"))} key={result._id}>
-				<a href={url} target="_blank">
-					<img className={this.bemBlocks.item("poster")} src={result._source.poster} width="180" height="270"/>
-					<div className={this.bemBlocks.item("title")}>{result._source.title}</div>
-				</a>
-			</div>
-		)
-	}
+const MovieHitsListItem = (props)=> {
+  const {bemBlocks, result} = props
+  let url = "http://www.imdb.com/title/" + result._source.imdbId
+  const source:any = _.extend({}, result._source, result.highlight)
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <div className={bemBlocks.item("poster")}>
+        <img data-qa="poster" className={bemBlocks.item("poster")} src={result._source.poster}/>
+      </div>
+      <div className={bemBlocks.item("details")}>
+        <a href={url} target="_blank"><h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></h2></a>
+        <h3 className={bemBlocks.item("subtitle")}>Released in {source.year}, rated {source.imdbRating}/10</h3>
+        <div className={bemBlocks.item("text")} dangerouslySetInnerHTML={{__html:source.plot}}></div>
+      </div>
+    </div>
+  )
 }
 
 export default class App extends React.Component {
 	render() {
 		return (
-		<div className="search-site">
+		<div className="sk-layout">
 			<SearchkitProvider searchkit={sk}>
 				<div>
-					<div className="search-site__query">
-						<SearchBox autofocus={true} searchOnChange={true} queryFields={["actors^1","type^2","languages","title^5", "genres^2"]}/>
+
+					<div className="sk-layout__top-bar sk-top-bar">
+				    <div className="sk-top-bar__content">
+							<SearchBox autofocus={true} searchOnChange={true} queryFields={["actors^1","type^2","languages","title^5", "genres^2"]}/>
+						</div>
 					</div>
 
-					<div className="search-site__results">
-						<MovieHits hitsPerPage={10}/>
+					<div className="sk-layout__body">
+
+						<div className="sk-layout__filters">
+							<RefinementListFilter id="actors" title="Genres" field="genres.raw" size={10}/>
+						</div>
+
+				    <div className="sk-layout__results sk-results-list">
+							<Hits hitsPerPage={10} itemComponent={MovieHitsListItem} mod="sk-hits-list"/>
+						</div>
+
 					</div>
+
 				</div>
 			</SearchkitProvider>
 		</div>
