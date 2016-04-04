bc84518ef9ae3a7a5185275e057f6e7b1c2a2b4c
Ash <ssetem@googlemail.com>
2016-04-04 13:17:12 +0100

    update boilerplate to 0.9

--- src/app.jsx
+++ src/app.jsx
@@ -2,67 +2,83 @@ import * as React from "react";
 import * as _ from "lodash";
 
 import {
-	SearchkitManager,
-	SearchkitProvider,
-	SearchBox,
-	RefinementListFilter,
-	Hits
+	SearchkitManager, SearchkitProvider,
+	SearchBox, RefinementListFilter, MenuFilter,
+	Hits, HitsStats, NoHits, Pagination, SortingSelector,
+	SelectedFilters, ResetFilters, ItemHistogramList,
+	Layout, LayoutBody, LayoutResults, TopBar,
+	SideBar, ActionBar, ActionBarRow
 } from "searchkit";
 
 require("./index.scss");
 
 const host = "http://demo.searchkit.co/api/movies"
-const sk = new SearchkitManager(host, {
-  multipleSearchers:false
-})
+const searchkit = new SearchkitManager(host)
 
-const MovieHitsListItem = (props)=> {
+const MovieHitsGridItem = (props)=> {
   const {bemBlocks, result} = props
   let url = "http://www.imdb.com/title/" + result._source.imdbId
   const source:any = _.extend({}, result._source, result.highlight)
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
-      <div className={bemBlocks.item("poster")}>
-        <img data-qa="poster" className={bemBlocks.item("poster")} src={result._source.poster}/>
-      </div>
-      <div className={bemBlocks.item("details")}>
-        <a href={url} target="_blank"><h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></h2></a>
-        <h3 className={bemBlocks.item("subtitle")}>Released in {source.year}, rated {source.imdbRating}/10</h3>
-        <div className={bemBlocks.item("text")} dangerouslySetInnerHTML={{__html:source.plot}}></div>
-      </div>
+      <a href={url} target="_blank">
+        <img data-qa="poster" className={bemBlocks.item("poster")} src={result._source.poster} width="170" height="240"/>
+        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}>
+        </div>
+      </a>
     </div>
   )
 }
 
-export default class App extends React.Component {
-	render() {
+export class App extends React.Component {
+	render(){
 		return (
-		<div className="sk-layout">
-			<SearchkitProvider searchkit={sk}>
-				<div>
-
-					<div className="sk-layout__top-bar sk-top-bar">
-				    <div className="sk-top-bar__content">
-							<SearchBox autofocus={true} searchOnChange={true} queryFields={["actors^1","type^2","languages","title^5", "genres^2"]}/>
-						</div>
-					</div>
-
-					<div className="sk-layout__body">
-
-						<div className="sk-layout__filters">
-							<RefinementListFilter id="actors" title="Genres" field="genres.raw" size={10}/>
-						</div>
-
-				    <div className="sk-layout__results sk-results-list">
-							<Hits hitsPerPage={10} itemComponent={MovieHitsListItem} mod="sk-hits-list"/>
-						</div>
-
-					</div>
-
-				</div>
-			</SearchkitProvider>
-		</div>
-
-		);
+			<SearchkitProvider searchkit={searchkit}>
+		    <Layout>
+		      <TopBar>
+		        <SearchBox
+		          autofocus={true}
+		          searchOnChange={true}
+							placeholder="Search movies..."
+		          prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
+		      </TopBar>
+		      <LayoutBody>
+		        <SideBar>
+							<MenuFilter
+								id="type"
+								title="Movie Type"
+								field="type.raw"
+								listComponent={ItemHistogramList}/>
+		          <RefinementListFilter
+		            id="actors"
+		            title="Actors"
+		            field="actors.raw"
+		            operator="AND"
+		            size={10}/>
+		        </SideBar>
+		        <LayoutResults>
+		          <ActionBar>
+		            <ActionBarRow>
+		              <HitsStats/>
+									<SortingSelector options={[
+										{label:"Relevance", field:"_score", order:"desc"},
+										{label:"Latest Releases", field:"released", order:"desc"},
+										{label:"Earliest Releases", field:"released", order:"asc"}
+									]}/>
+		            </ActionBarRow>
+		            <ActionBarRow>
+		              <SelectedFilters/>
+		              <ResetFilters/>
+		            </ActionBarRow>
+		          </ActionBar>
+		          <Hits mod="sk-hits-grid" hitsPerPage={10} itemComponent={MovieHitsGridItem}
+		            sourceFilter={["title", "poster", "imdbId"]}/>
+		          <NoHits/>
+							<Pagination showNumbers={true}/>
+		        </LayoutResults>
+		      </LayoutBody>
+		    </Layout>
+		  </SearchkitProvider>
+		)
 	}
 }
