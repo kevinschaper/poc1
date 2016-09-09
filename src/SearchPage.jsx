e86efd3f87ee0e09b5ef2bdc4f8a71fbfb817063
Travis Sheppard <tshepp@stanford.edu>
2016-09-09 14:35:06 -0700

    better highlighting

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -19,16 +19,26 @@ const GeneItem = (props)=> {
 	let data = props.result._source;
   const {bemBlocks, result} = props
   const source:any = _.extend({}, result._source, result.highlight)
-  console.log(result.highlight)
         //   <img data-qa="poster" className={bemBlocks.item("poster")} src={result._source.poster} width="170" height="240"/>
         // <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}>
 
   return (
     <div data-qa="hit">
-		<a target='_new' href={source.href} dangerouslySetInnerHTML={{__html:source.gene_symbol}}/>
-		<p dangerouslySetInnerHTML={{__html:source.gene_name}}/>
+		<a target='_new' href={source.href} dangerouslySetInnerHTML={{__html:source.gene_symbol}} />
+		<dl>
+			<dt>Organism</dt>
+			<dd dangerouslySetInnerHTML={{__html:source.organism}}></dd>
+		</dl>
+		<dl>
+			<dt>Gene Name</dt>
+			<dd dangerouslySetInnerHTML={{__html:source.gene_name}}></dd>
+		</dl>
+		<dl>
+			<dt>Description</dt>
+			<dd dangerouslySetInnerHTML={{__html:source.description}}></dd>
+		</dl>
     </div>
-  )
+  );
 }
 
 
@@ -43,10 +53,11 @@ export class SearchPage extends React.Component {
 			    <Layout>
 			      <TopBar>
 			        <SearchBox
-			          autofocus={true}
-			          searchOnChange={true}
-					placeholder="Search movies..."
-			          prefixQueryFields={["gene_name", "description", "secondary_id", "gene_symbol^10"]}/>
+						autofocus={true}
+						searchOnChange={true}
+						placeholder="Search genes..."
+						prefixQueryFields={["gene_name", "description", "secondary_id", "organism", "gene_symbol^10"]}
+					/>
 			      </TopBar>
 			      <LayoutBody>
 			        <SideBar>
@@ -69,18 +80,13 @@ export class SearchPage extends React.Component {
 			          <ActionBar>
 			            <ActionBarRow>
 			              <HitsStats/>
-										<SortingSelector options={[
-											{label:"Relevance", field:"_score", order:"desc", defaultOption:true},
-											{label:"Latest Releases", field:"released", order:"desc"},
-											{label:"Earliest Releases", field:"released", order:"asc"}
-										]}/>
 			            </ActionBarRow>
 			            <ActionBarRow>
 			              <SelectedFilters/>
 			              <ResetFilters/>
 			            </ActionBarRow>
 			          </ActionBar>
-			          <Hits mod="sk-hits-list" hitsPerPage={10} itemComponent={GeneItem} highlightFields={["gene_name", "description", "secondary_id"]}
+			          <Hits mod="sk-hits-list" hitsPerPage={10} itemComponent={GeneItem} highlightFields={["gene_name", "description", "secondary_id", "organism", "gene_symbol"]}
 			            />
 			          <NoHits/>
 						<Pagination showNumbers={true}/>
